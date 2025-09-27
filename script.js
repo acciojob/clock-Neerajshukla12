a90016bd581cbac36e1e36b5759c0f1834813e02
Neeraj Kumar Shukla <132157056+Neerajshukla12@users.noreply.github.com>
2025-09-27 19:29:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-//your JS code here. If required.
  function updateTimer() {
-      let now = new Date();
-      let options = {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
-      };
-      document.getElementById('timer').textContent = now.toLocaleString('en-US', options);
+      // let now = new Date();
+      // let  dateTime = now.toString();
+      document.getElementById("timer").innerHTML =new Date();
     }
     updateTimer();
     setInterval(updateTimer, 1000);
